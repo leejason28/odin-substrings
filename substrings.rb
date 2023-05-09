b07f1eb4e97f06a7c54973f08eec00ab83ce4ca6
leejason28 <jason928lee@gmail.com>
2023-05-09 11:32:02 -0700

    done

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,16 @@
-def substrings(word, dictionary)
-
+def substrings(string, dictionary)
+  string_array = string.downcase.split
+  result = Hash.new(0)
+  string_array.each do |word|
+    dictionary.reduce(result) do |result, substring|
+      result[substring] += 1 if word.include?(substring)
+      result
+    end
+  end
+  result
 end
+
+##
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# p substrings("below", dictionary)
+# p substrings("Howdy partner, sit down! How's it going?", dictionary)
